41e7fc99ac4d45d440481c75783d6e61981ed4de
Awais-Gilal <141895482+Awais-Gilal@users.noreply.github.com>
2023-08-10 16:10:27 +0000

    Templates modified
    Added route / => home
    /pontact => contact page
    /post => post gase

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ app = Flask(__name__)
 #routing home page
 @app.route("/")
 def home():
-  return render_template()
+  return render_template("index.html")
+
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+@app.route("/post")
+def post():
+  return render_template("post.html")
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=81, debug=True)

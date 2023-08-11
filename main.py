f126729501268c97170bc23091580aec88b1cadf
Awais-Gilal <141895482+Awais-Gilal@users.noreply.github.com>
2023-08-11 10:19:34 +0000

    Fixed templates error

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+  return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
+
+@app.route("/post")
+def post():
+  return render_template("post.html")
+
 
 app.run(host='0.0.0.0', port=81)

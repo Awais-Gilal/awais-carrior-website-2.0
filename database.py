263b59b8dacc51c03ae6c63ac31cbf4d511d6c70
Awais-Gilal <141895482+Awais-Gilal@users.noreply.github.com>
2023-08-14 09:00:32 +0000

    modified

---
+++ database.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, text, insert
+
+
+def tup_into_dict_contact(tup):
+  return {
+    'sno': tup[0],
+    'name': tup[1],
+    'email': tup[2],
+    'phone': tup[3],
+    'msg': tup[4],
+    'time': tup[5]
+  }
+def tup_into_dict_job(tup):
+  return {
+    "id": tup[0],
+    "title": tup[1],
+    "salary": tup[2],
+    "corrency": tup[3],
+    "location": tup[4],
+    "responsibility": tup[5],
+    "requirements": tup[6],
+    "time": tup[7],
+  }
+
+username = "sql6639351"
+password = "WCrfFr7VGg"
+host = "sql6.freesqldatabase.com"
+database_name = "sql6639351"
+charset = "utf8mb4"
+
+url = f"mysql+pymysql://{username}:{password}@{host}/{database_name}?charset={charset}"
+# mysql+pymysql://sql6639351:WCrfFr7VGg@sql6.freesqldatabase.com/sql6639351?charset=utf8mb4
+
+engine = create_engine(url)
+
+
+def load_jobs():
+  result_dict = []
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from job"))
+    for row in result:
+      result_dict.append(tup_into_dict_job(row))
+    return result_dict
+
+
+if __name__ == "__main__":
+  
+  print(load_jobs())
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
 import json
+from database import load_jobs
 
-app = Flask(__name__)
-#getting data from json
-with open("templates/config/config.json") as f:
-  config_data = json.load(f)
-  JOBS = config_data["JOBS"]
-  CONTACT = config_data["contact"]
-
+#getting datafrom json
+#with open("templates/config/config.json") as f:
+  #config_data = json.load(f)
+  #JOBS = config_data["JOBS"]
 
 #function for adding data in json
 def save_in_dic(dic, name, email, phone, message):
@@ -17,6 +15,8 @@ def save_in_dic(dic, name, email, phone, message):
     "phone": phone,
     "message": message
   })
+  
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -37,15 +37,13 @@ def contact():
     name = request.form.get("name")
     email = request.form.get("email")
     phone = request.form.get("phone")
-    msg = request.form.get("message")
-    save_in_dic(config_data, name, email, phone, msg)
-    with open("templates/config/config.json", "w") as a:
-      json.dump(config_data, a)
+    msg = request.form.get("messaage")
   return render_template("contact.html")
 
 
 @app.route("/post")
 def post():
+  JOBS = load_jobs()
   return render_template("post.html", jobs=JOBS)
 
 

62c96a410ba056af88283c3a08506e191261d2cf
Awais-Gilal <141895482+Awais-Gilal@users.noreply.github.com>
2023-08-25 05:00:25 +0000

    uploading data to main git

--- database.py
+++ database.py
@@ -1,30 +1,39 @@
-from sqlalchemy import create_engine, text, insert
+from sqlalchemy import create_engine, text
+import datetime
 
 
-def tup_into_dict_contact(tup):
-  return {
-    'sno': tup[0],
-    'name': tup[1],
-    'email': tup[2],
-    'phone': tup[3],
-    'msg': tup[4],
-    'time': tup[5]
-  }
+#other functions
+def month_str(value):
+  month = int(value[1])
+  if month == 1:
+    return "January"
+  elif month == 2:
+    return "February"
+  elif month == 3:
+    return "March"
+  elif month == 4:
+    return "April"
+  elif month == 5:
+    return "May"
+  elif month == 6:
+    return "June"
+  elif month == 7:
+    return "July"
+  elif month == 8:
+    return "August"
+  elif month == 9:
+    return "September"
+  elif month == 10:
+    return "Octiber"
+  elif month == 11:
+    return "November"
+  elif month == 12:
+    return "December"
+  else:
+    return None
 
 
-def tup_into_dict_job(tup):
-  return {
-    "id": tup[0],
-    "title": tup[1],
-    "salary": tup[2],
-    "corrency": tup[3],
-    "location": tup[4],
-    "responsibility": tup[5],
-    "requirements": tup[6],
-    "time": tup[7],
-  }
-
-
+#connecting with database
 username = "sql6639351"
 password = "WCrfFr7VGg"
 host = "sql6.freesqldatabase.com"
@@ -32,20 +41,123 @@ database_name = "sql6639351"
 charset = "utf8mb4"
 
 url = f"mysql+pymysql://{username}:{password}@{host}/{database_name}?charset={charset}"
-# mysql+pymysql://sql6639351:WCrfFr7VGg@sql6.freesqldatabase.com/sql6639351?charset=utf8mb4
 
 engine = create_engine(url)
 
 
+#=>loading data from database
+#load jobs from database
 def load_jobs():
-  result_dict = []
+  """loading jobs data form database and returning as list of dictingaries"""
   with engine.connect() as conn:
-    result = conn.execute(text("select * from job"))
-    for row in result:
-      result_dict.append(tup_into_dict_job(row))
-    return result_dict
+    data = conn.execute(text("SELECT * FROM job"))
+    list_data = []
+    for dic in data:
+      list_data.append({
+        "id": dic[0],
+        "title": dic[1],
+        "salary": dic[2],
+        "currency": dic[3],
+        "location": dic[4],
+        "responsibility": dic[5],
+        "requirements": dic[6]
+      })
+    return list_data
+
+
+#liadimg contacts  from database
+def load_contacts():
+  """loading messages data form database and returning as list of dictingaries"""
+  contact_list_data = []
+  with engine.connect() as conn:
+    contact_data = conn.execute(text("SELECT * FROM contact"))
+    for dic in contact_data:
+      contact_list_data.append({
+        "sno": dic[0],
+        "name": dic[1],
+        "email": dic[2],
+        "phone": dic[3],
+        "msg": dic[4],
+        "date": dic[5]
+      })
+  return contact_list_data
+
+#get data from jobs
+def load_posts():
+  posts = []
+  with engine.connect() as conn:
+    database_posts = conn.execute(text("SELECT * FROM `posts`"))
+  for post in database_posts:
+    date = post[5]
+    date = str(date).split(' ')
+    date = date[0].split('-')
+    post_data = {
+      "sno": post[0],
+      "post_title": post[1],
+      "post": post[2],
+      "name": post[3].title(),# modified and not checked check it when error raise
+      "email":  post[4],
+      "day": date[2],
+      "month": month_str(date[1]),
+      "year": date[0]
+    }
+    posts.append(post_data)
+  return posts
+
+
+
+#inserting data into database
+def insert_into_contact(dic):
+  """inserting data into database required dictionary in argument"""
+  date_time = datetime.datetime.now()
+  current_datetime = date_time.striftime("%d-%m-%Y %H:%M:%S")
+  with engine.connect() as conn:
+    conn.execute(
+      text(
+        f"INSERT INTO `contact` (`sno`,`name`,`email`,`phone`,`msg`,`date`) VALUES (NULL, '{dic['name']}','{dic['email']}','{dic['phone']}','{dic['msg']}','{current_datetime}');"
+      ))
+    conn.commit()
+
+
+def insert_into_job(dic):
+  with engine.connect() as conn:
+    conn.execute(
+      text(
+        f"INSTER INTO `job` (`id`,`title`,`salary`,`currency`,`location`,`requirements`,`responsibility`) VALUES (NULL, '{dic['title']}','{dic['salary']}','{dic['currency']}','{dic['location']}','{dic['requirements']}','{dic['responsibility']}');"
+      ))
+    conn.commit()
+
+
+
+
+#inserting posts data into database
+def insert_into_posts(dic):
+  with engine.connect() as conn:
+    time = datetime.datetime.now()
+    time = str(time)
+    conn.execute(
+      text(
+        f"INSERT INTO `posts` (`sno`, `post_title`, `post`, `name`,`email`, `Datetime`) VALUES(NULL, '{dic['post_title']}', '{dic['post']}', '{dic['name']}','{dic['email']}' ,'{time}');"
+      ))
+    conn.commit()
 
 
 if __name__ == "__main__":
+  pass
+  # contact_data = {
+  #   'name': "ali",
+  #   "email": "ali@gmail.com",
+  #   "phone": 92010,
+  #   "msg": "this is best website"
+  # }
+  #insert_into_contact(contact_data)
 
-  print(load_jobs())
+  # data = {
+  #   "name": "Muzamil",
+  #   "post_title": "Failure is not an option",
+  #   "post":
+  #   "Many say exploration is part of our destiny, but it’s actually our duty to future generations."
+  # }
+  #insert_into_posts(data)
+
+  #print(load_posts())
--- main.py
+++ main.py
@@ -1,47 +1,69 @@
 from flask import Flask, render_template, request
-import json
-from database import load_jobs
+from database import load_posts, insert_into_posts, load_jobs, insert_into_job, insert_into_contacts
 
-
-#function for adding data in json
-def save_in_dic(dic, name, email, phone, message):
-  dic['contact'].append({
-    "name": name,
-    "email": email,
-    "phone": phone,
-    "message": message
-  })
-  
+# initilizing flask app
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+#creating route for home page.
+@app.route('/home')
+@app.route("/")
+def home():
+  posts = load_posts()
+  return render_template("index.html", posts=posts)
 
 
+@app.route("/posts/form", methods=["POST", "GET"])
+def post_form():
+  if request.method == "POST":
+    data = {
+      "name": request.form.get("name"),
+      "post_title": request.form.get("post_title"),
+      "post": request.form.get("post"),
+      "email": request.form.get("email")
+    }
+    insert_into_posts(data)
+  return render_template("post_form.html")
+
+
+#creating about page
 @app.route("/about")
 def about():
-  return render_template('about.html')
+  return render_template("about.html")
 
 
-#route contact
+#creating contact page
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-
   if request.method == "POST":
-    name = request.form.get("name")
-    email = request.form.get("email")
-    phone = request.form.get("phone")
-    msg = request.form.get("messaage")
-    
+    data = {
+      "name":request.form.get("name"),
+      "email":request.form.get("email"),
+      "phone":request.form.get("phone"),
+      "message":request.form.get("message")
+    }
   return render_template("contact.html")
 
 
-@app.route("/post")
-def post():
-  JOBS = load_jobs()
-  return render_template("post.html", jobs=JOBS)
+@app.route("/jobs")
+def home_jobs():
+  jobs = load_jobs()
+  return render_template("home_jobs.html", jobs=jobs)
+
+
+@app.route("/jobs/form", methods=["GET", "POST"])
+def jobs_form():
+  if request.method == "POST":
+    data = {
+      "title": request.form.get('title'),
+      "salary": request.form.get('salary'),
+      "currency": request.form.get("currency"),
+      "location": request.form.get("location"),
+      "requirements": request.form.get("requirements"),
+      "responsibility": request.form.get("responsibility")
+    }
+    insert_into_job(data)
+  return render_template("job_form.html")
 
 
 if __name__ == "__main__":
